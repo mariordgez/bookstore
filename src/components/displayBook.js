3d521301da93f20bb5f0e0749324d5a0fdef5dc7
mariordgez <mariordgez@hotmail.com>
2021-10-01 21:27:47 -0700

    changes to display book

--- src/components/displayBook.js
+++ src/components/displayBook.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import '../App.css';
+import { useSelector } from 'react-redux';
 
-const DisplayBook = () => (
-  <div>
-    <h3>Book Title</h3>
-    <button type="submit" aria-label="Decrement value">
-      remove
-    </button>
-  </div>
-);
+const DisplayBook = () => {
+  const books = useSelector((state) => state.booksReducer);
+  Object.keys(books).forEach((key) => {
+    console.log(books[key].author);
+    const { title, author } = books[key];
+    return (
+      <div>
+        <p>{title}</p>
+        <p>{author}</p>
+        <button type="submit" aria-label="Decrement value">
+          remove
+        </button>
+      </div>
+    );
+  });
+};
 
 export default DisplayBook;
